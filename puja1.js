490a918126e63cc52c99b05ecb76d1b02a257e6e
Mahi Tyagi <mahityagi2003@gmail.com>
2024-11-01 21:05:02 +0530

    Initial commit

---
+++ puja1.js
@@ -0,0 +1,36 @@
+
+document.querySelectorAll(".faq-question").forEach(button => {
+    button.addEventListener("click", () => {
+      button.classList.toggle("active");
+      const answer = button.nextElementSibling;
+      if (answer.style.display === "block") {
+        answer.style.display = "none";
+      } else {
+        answer.style.display = "block";
+      }
+    });
+  });
+  
+
+const countdown = () => {
+    const endDate = new Date("October 31, 2024 00:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = endDate - now;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.getElementById('days').innerText = textDay;
+    document.getElementById('hours').innerText = textHour;
+    document.getElementById('minutes').innerText = textMinute;
+    document.getElementById('seconds').innerText = textSecond;
+};
+
+setInterval(countdown, 1000);
